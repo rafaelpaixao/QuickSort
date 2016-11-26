0f4e900916ba71729ce2390fab9650d714ccef8c
rafaelpaixao <rafaelsspaixao@gmail.com>
2016-11-26 13:25:42 -0300

    Versão inicial com ordenação de vetor de inteiros e saída em texto.

--- src/quicksort/QuickSort.java
+++ src/quicksort/QuickSort.java
@@ -1,21 +1,76 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package quicksort;
 
-/**
- *
- * @author Rafael
- */
+import java.util.Random;
+
 public class QuickSort {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Random aleatorio = new Random();
+    static int contIteracoes = 0;
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[] vetor = gerarVetorAleatorio(10, 50);
+
+        imprimirVetor(vetor);
+
+        quickSort(vetor, 0, vetor.length - 1);
+
+        imprimirVetor(vetor);
+    }
+
+    public static void quickSort(int vetor[], int indexInicial, int indexFinal) {
+        int indexPivo = (indexInicial + indexFinal) / 2;
+        int valorPivo = vetor[indexPivo];
+        int aux;
+        int i = indexInicial;
+        int j = indexFinal;
+
+        System.out.println("\nIteração: " + ++contIteracoes);
+        System.out.print("Vetor recebido: ");
+        imprimirVetor(vetor);
+        System.out.println("Pivo: " + valorPivo);
+
+        while (i < indexPivo && j > indexPivo) {
+            while (vetor[j] >= valorPivo && j > indexPivo) {
+                j--;
+            }
+            while (vetor[i] <= valorPivo && i < indexPivo) {
+                i++;
+            }
+            if (i < j) {
+                System.out.println("Trocando: " + vetor[i] + " com " + vetor[j]);
+                aux = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = aux;
+            }
+        }
+
+        System.out.print("Vetor entregue: ");
+        imprimirVetor(vetor);
+
+        if (indexInicial < indexPivo - 1) {
+            quickSort(vetor, indexInicial, indexPivo - 1);
+        }
+        if (indexFinal > indexPivo + 1) {
+            quickSort(vetor, indexPivo + 1, indexFinal);
+        }
     }
     
+
+    public static void imprimirVetor(int vetor[]) {
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println("");
+    }
+
+    public static int[] gerarVetorAleatorio(int tamanho, int limite) {
+        int[] v = new int[tamanho];
+
+        for (int i = 0; i < v.length; i++) {
+            v[i] = aleatorio.nextInt(limite);
+        }
+
+        return v;
+    }
+
 }
